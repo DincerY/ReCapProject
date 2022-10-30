486c01e58b4df757a4e8a5ba852fc20d866f4170
Dinçer Yiğit <dinceryigit60@gmail.com>
2022-10-30 16:11:08 +0300

    Add project files.

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,53 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CarManager:ICarService
+    {
+        private ICarDal _carDal;
+        public CarManager(ICarDal carDal)
+        {
+            _carDal=carDal;
+        }
+        //public void Delete(Car car)
+        //{
+        //    _carDal.Delete(car);
+        //}
+        public void Add(Car car)
+        {
+            _carDal.Add(car);
+        }
+        public void Delete(Car car)
+        {
+            _carDal.Delete(car);
+        }
+        public void GetAllById(int Id)
+        {
+            foreach (var item in _carDal.GetAllById(Id))
+            {
+                Console.WriteLine(item.Description);
+                Console.WriteLine(item.ModelYear); 
+                Console.WriteLine(item.ColorId);
+            }
+        }
+        public void Update(Car car)
+        {
+            _carDal.Update(car);
+        }
+        public List<Car> GetAll()
+        {
+            return _carDal.GetAll();
+        }
+       
+    }
+}
---
+++ ReCapProject2/Program.cs
@@ -0,0 +1,33 @@
+﻿using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReCapProject2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            carManager.Add(new Car() { Id = 4, BrandId = 2, ColorId = 10, ModelYear = 2013, DailyPrice = 800, Description = "New" });
+            //carManager.Delete(new Car() { Id = 1 });
+            //carManager.Delete(new Car() { Id = 2 });
+            //carManager.GetAllById(3);
+
+
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Description);
+                Console.WriteLine(car.ModelYear);
+            }
+            Console.ReadLine();
+           
+        }
+    }
+}
